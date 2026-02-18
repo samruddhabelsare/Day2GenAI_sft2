4ecf3a2d5db077d0a3829da40c9df9c88b56e6cc
Samruddha Belsare <ujjwalabelsare123@gmail.com>
2026-02-18 15:46:54 +0530

    C++ Based RAG chatbot

--- rag.py
+++ rag.py
@@ -18,23 +18,53 @@ st.set_page_config(page_title="C++ Rag Chatbot", page_icon="☠️")
 st.title(" ☠️ C++ RAG chatbot")
 st.write("Write all questions about C++ only ")
 
-#step 2 last environment variables
+# step 2 --> load environment variables
 load_dotenv()
 
-#step 3 --> use cache (importance for performance) to load and process documents
+# step 3 --> use cache (importance for performance) to load and process documents
 @st.cache_resource
-#streamlit decorator -run this function only once ,not every refresh
-#this is very important for embeddings + FAISS speed
+# streamlit decorator - run this function only once , not every refresh
+# this is very important for embeddings + FAISS speed
 
-#MAIN RAG PIPELINE
+# MAIN RAG PIPELINE
 def load_vectorstore():
     
-    #load documents 
-    loader = TextLoader("C++_Introduction.txt",encoding="utf-8")
+    # load documents 
+    loader = TextLoader("C++_Introduction.txt", encoding="utf-8")
     documents = loader.load()
     
     # split documents into chunks 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200,chunk_overlap=20)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=200,
+        chunk_overlap=20
+    )
     final_docs = text_splitter.split_documents(documents)
     
-    # embeddings
+    # create embeddings
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+    
+    # create FAISS vector store
+    db = FAISS.from_documents(final_docs, embeddings)
+    
+    return db
+
+
+# load vector store once
+db = load_vectorstore()
+
+
+# step 5 --> user input
+query = st.text_input("Ask any question related to C++")
+
+if query:
+    # condition - find top 3 most similar chunks from FAISS
+    docs = db.similarity_search(query, k=3)
+    
+    st.subheader("Retrieved content : 🟨")
+    
+    for i, doc in enumerate(docs):
+        st.markdown(f"**Result {i+1}:**")
+        st.write(doc.page_content)
+        st.markdown("---")
