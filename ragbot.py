03473b5567c1cf1e0e221074fad58f16daeeaa67
Samruddha Belsare <ujjwalabelsare123@gmail.com>
2026-02-18 16:22:24 +0530

    MAIN RAG PIPELINE , load LLM , chat interface

--- ragbot.py
+++ ragbot.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 import streamlit as st
 import os 
 
@@ -6,9 +7,63 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
 
 # step 1 --> Page configuration 
 
 st.set_page_config(page_title="C++ Rag Chatbot", layout="wide")
 st.title(" ☠️ C++ RAG chatbot using ollama ")
+
+# step 2 --> load environment variables
+load_dotenv()
+
+# step 3 --> use cache (importance for performance) to load and process documents
+@st.cache_resource
+# streamlit decorator - run this function only once , not every refresh
+# this is very important for embeddings + FAISS speed
+
+# MAIN RAG PIPELINE
+def load_vectorstore():
+    
+    # load documents 
+    loader = TextLoader("C++_Introduction.txt", encoding="utf-8")
+    documents = loader.load()
+    
+    # split documents into chunks 
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=200,
+        chunk_overlap=20
+    )
+    final_docs = text_splitter.split_documents(documents)
+    
+    # create embeddings
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+    
+    # create FAISS vector store
+    db = FAISS.from_documents(final_docs, embeddings)
+    
+    return db
+
+db= load_vectorstore
+
+# extra stop - load LLM
+llm=Ollama("model=gemma2:2b")
+
+# last step - chat interface 
+user_question=st.text_input("Ask question about C++")
+
+if user_question:
+    with st.spinner("RUk ja bhai , sochne de ....."):
+        docs=db.similarity_search(user_question)
+        
+        # combine cotext (construction)
+        # 1. Extract text from retrived documents
+        # 2. Joins them into single string 
+        # 3. this becomes context of LLM 
+        
+        context = "\n ".join([doc.page_content for doc in docs])
+        
+        
+    
